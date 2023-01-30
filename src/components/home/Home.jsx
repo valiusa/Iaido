8af9c69e4f55f06d05903b9b6991c0aae9b74b4b
Valentin Vladimirov <valentinvladimirov@abv.bg>
2023-01-31 00:58:06 +0200

    added home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// styles
+import classNames from "classnames";
+import styles from "./Home.module.scss";
+
+export default function Home() {
+    return <div className={classNames(styles.home_container)}></div>;
+}
