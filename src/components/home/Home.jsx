3c468bc885febb642bcd4462624323b3d8fe1e71
Valentin Vladimirov <valentinvladimirov@abv.bg>
2023-01-31 14:46:39 +0200

    added Home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+// componets
+import SocialMedias from "../social_medias/SocialMedias";
+
 // styles
 import classNames from "classnames";
 import styles from "./Home.module.scss";
 
 export default function Home() {
-    return <div className={classNames(styles.home_container)}></div>;
+    return (
+        <div className={classNames(styles.home_container)}>
+            <SocialMedias />
+        </div>
+    );
 }
---
+++ src/components/social_medias/SocialMedias.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+// font awesome icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faInstagram,
+    faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+// styles
+import classNames from "classnames";
+import styles from "./SocialMedias.module.scss";
+
+export default function SocialMedias() {
+    return (
+        <div className={classNames(styles.social_medias_container)}>
+            <div className={classNames(styles.social_media_btn_container)}>
+                <a
+                    href="https://www.facebook.com/IaidoClubSofia"
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon
+                        style={{ color: "white" }}
+                        icon={faFacebook}
+                        size={"2x"}
+                    />
+                </a>
+            </div>
+            <div className={classNames(styles.social_media_btn_container)}>
+                <a
+                    href="https://www.instagram.com/"
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon
+                        style={{ color: "white" }}
+                        icon={faInstagram}
+                        size={"2x"}
+                    />
+                </a>
+            </div>
+            <div className={classNames(styles.social_media_btn_container)}>
+                <a
+                    href="https://www.youtube.com/"
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon
+                        style={{ color: "white" }}
+                        icon={faYoutube}
+                        size={"2x"}
+                    />
+                </a>
+            </div>
+        </div>
+    );
+}
